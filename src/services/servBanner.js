aeebda9d167c2c3d32a0e879c601a2e34adbab5f
JoseRCL1981 <jribamarcl@hotmail.com>
2026-06-10 21:20:42 -0300

    Adição banco

---
+++ src/services/servBanner.js
@@ -0,0 +1,8 @@
+import repositoryBanner from "../repositories/repoBanner.js"
+async function Listar() {
+    
+    const banners = await repositoryBanner.Listar();
+    return banners;
+}
+
+export default { Listar };
---
+++ src/services/servEmpresa.js
@@ -0,0 +1,8 @@
+import repositoryEmpresa from "../repositories/repoEmpresa.js"
+async function Listar() {
+    
+    const empresas = await repositoryEmpresa.Listar();
+    return empresas;
+}
+
+export default { Listar };
